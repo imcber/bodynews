c6afaf10acb39bcc15b32ddfe72730aaf7e3a9ba
imcber <imcber1@gmail.com>
2016-08-04 18:59:38 -0500

    Creamos post y lo muestra en la principal

--- bodynews/urls.py
+++ bodynews/urls.py
@@ -7,5 +7,6 @@ from posts import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/$', views.ListView.as_view(), name="lista"),
+    url(r'^blog/(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name="detalle"),
     url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import View
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
 
 class ListView(View):
@@ -23,8 +25,15 @@ class DetailView(View):
 		}
 		return render(request,template_name,context)
 
-def post_new(request):
-	form = PostForm()
-	template_name = 'posteando.html'
-	context =  {'form': form}
-	return render(request, template_name, context)
+def post_new(request):#Permite guardar nuevos post en la base de datos que se crea en /post/new#}
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('/blog/', pk=post.pk)#al terminar el post nos envia a la principal#}
+	else:
+		form = PostForm()
+	return render(request, 'posteando.html', {'form': form})
