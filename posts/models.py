17746dde44048a69e952ec7a5a2cd8adddabf641
imcber <imcber1@gmail.com>
2016-08-03 19:32:51 -0500

    blog principal

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+	titulo = models.CharField(max_length=140)
+	fecha = models.DateTimeField(auto_now=True)
+	cuerpo = models.TextField()
+	publicado = models.BooleanField(default=False)
+	slug = models.SlugField(max_length=500,blank=True,null=True) #esto es un slug
+
+	def __str__(self):
+		return self.titulo
+
+	def get_absolute_url(self):
+		return reverse('detalle',args=[self.slug])
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Post
 
-# Create your views here.
+
+
+class ListView(View):
+	def get(self,request):
+		template_name = 'lista.html'
+		posts = Post.objects.all().order_by('titulo')
+		compa = {
+		'posts':posts,
+		}
+		return render(request,template_name,compa)
+
+class DetailView(View):
+	def get(self,request,slug):
+		template_name = 'detalle.html'
+		post = Post.objects.get(slug=slug)
+		context = {
+		'post':post
+		}
+		return render(request,template_name,context)
